b5ea004579bed2958783cd8b9b7118d35b65c771
nsharma28 <125905409+nsharma28@users.noreply.github.com>
2023-10-23 18:12:09 +0530

    Update dataservice.py

--- service/dataservice.py
+++ service/dataservice.py
@@ -1,11 +1,15 @@
-from models.samplemodel import SampleModel
+from models.samplemodel import *
+from common.rdb_dal.sql import Sql
 from config import sql_query
+from common.MyConvert import *
 
 
 class DataService:
-    def get()->list[SampleModel]:
-        pass
+    def get(searchModel:searchModel())->list[SampleModel]:
+        sql = Sql('default')
+        return sql.execute_list(SampleModel,sql_query.property_get_by_id_query, MyConvert.to_dict(searchModel))
     
     
     def insert(SampleModel:SampleModel()):
-        pass
+        sql = Sql('default')
+        return sql.execute(sql_query.propert_insert_query, MyConvert.to_dict(SampleModel))
