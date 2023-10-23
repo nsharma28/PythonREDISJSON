e1023af048b59357652a3afa5d1fd5f3bf39a791
nsharma28 <125905409+nsharma28@users.noreply.github.com>
2023-10-23 18:10:33 +0530

    Update samplemodel.py

--- models/samplemodel.py
+++ models/samplemodel.py
@@ -4,3 +4,7 @@ from fastapi_camelcase import CamelModel
 class SampleModel(CamelModel,BaseModel):
     id:int = 0
     name:str = ''
+    is_active:bool = 1
+    
+class searchModel(CamelModel,BaseModel):
+    property_id:str = ''
