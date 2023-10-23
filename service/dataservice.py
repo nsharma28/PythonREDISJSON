64a3ea862692f100ef5b64d94e371d0b50af8d24
nsharma28 <125905409+nsharma28@users.noreply.github.com>
2023-10-23 17:01:18 +0530

    Add files via upload

---
+++ service/dataservice.py
@@ -0,0 +1,11 @@
+from models.samplemodel import SampleModel
+from config import sql_query
+
+
+class DataService:
+    def get()->list[SampleModel]:
+        pass
+    
+    
+    def insert(SampleModel:SampleModel()):
+        pass
