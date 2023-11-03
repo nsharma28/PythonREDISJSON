2833e59e56bc2ccb8d05a2bc832fb32952ab7123
nsharma28 <125905409+nsharma28@users.noreply.github.com>
2023-11-03 13:13:06 +0530

    Update common_functions.py

--- common/rdb_dal/common_utility/common_functions.py
+++ common/rdb_dal/common_utility/common_functions.py
@@ -1,3 +1,5 @@
+
+
 def retrieve_sql_database_record(connection_obj,query_string,param_dict,model_type = None):
     """ Execute the SQL COMMAND and convert the row data as  LIST ->  DataFrame 
 
@@ -65,5 +67,27 @@ def execute_dml(connection_obj,query_string,param_dict):
     else:
         cursor.execute(query_string,param_dict) # Execute the update/delete/insert
     connection_obj.commit() #Commit changes
+    return 0
+
+def execute_dml_batch(connection_obj,query_string,param_dict):
+    """ Execute SQL Command  Update , Insert or Delete operations. It doesn't return any data from the database
+
+    Args:
+        connection_obj (Object) : Connection object
+        query_string (String)   : Sql query 
+        param_dict (Dictonery)  : If Query do have parameters then it contain the key and its associate value
+
+    Returns:
+        Integer: Return 0 if query successfully executed
+    """
+    cursor = connection_obj.cursor() # Open the cursor.
+    
+    
+
+    if param_dict is None:
+        cursor.executemany(query_string)
+    else:
+        cursor.executemany(query_string,param_dict) # Execute the update/delete/insert
+    #connection_obj.commit() #Commit changes
     return 0
 
